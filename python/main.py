a44c6d5ce102cd9c9f5b0bc824ac468487179a2a
rachwalk <kajetan.rachwal@mailfence.com>
2021-01-20 17:23:52 +0100

    fixed conflicts

--- python/main.py
+++ python/main.py
@@ -2,13 +2,6 @@ from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Dense
 
-<<<<<<< HEAD
 from data_prep import neighbourhood as nb
 from data_prep import parser
 
-
-=======
-from data_prep import neighbourhood
-from data_prep import parser
-
->>>>>>> fabd34df02fc44a0b085b7561e3c1321adddfd4e
