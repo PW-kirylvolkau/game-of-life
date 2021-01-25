Added functions for a recurrence normalization which fixes the process of prediction

python/predict.py
@@ -1,4 +1,5 @@
 import numpy as np
+import normalizers as norms
 import tensorflow as tf
 from tensorflow import keras
 from data_prep import neighbourhood as nb
@@ -6,20 +7,9 @@ import csv
 
 def normalize_tensor(tensorobject):
     tensor_array = tensorobject.numpy()
-    #print(tensor_array)
-    for i in range(tensorobject.shape[0]):
-        for j in range(tensorobject.shape[1]):
-            for k in range(tensorobject.shape[2]):
-                for l in range(tensorobject.shape[3]):
-                    if tensor_array[i][j][k][l] < 0.01:
-                        tensor_array[i][j][k][l] = 0
-                    else:
-                        tensor_array[i][j][k][l] = 1
-    #print(tensor_array)
+    tensor_array = norms.recursive_normalize(tensor_array)
     modified_tensor = tf.convert_to_tensor(tensor_array, dtype=tf.int32)
     return modified_tensor
-
-
 
 
 def runPrediction(board, models, size):
@@ -34,9 +24,8 @@ def runPrediction(board, models, size):
             counter = 1
             for model in models:
                 input_neigh = model(input_neigh)
-                if counter == 1:
-                    input_neigh = normalize_tensor(input_neigh)
-                counter = counter + 1
+                input_neigh = normalize_tensor(input_neigh)
+                print(input_neigh)
             predicted[i,j] = input_neigh
     return predicted
 